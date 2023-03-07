df24c6a41cc482fc385b94331afb3535a63a72b8
iFedorov <igor170701@gmail.com>
2023-03-07 15:15:39 +0300

    CORS enabled

--- src/main/java/ru/igor17/chadpizza/controller/CustomerController.java
+++ src/main/java/ru/igor17/chadpizza/controller/CustomerController.java
@@ -3,6 +3,7 @@ package ru.igor17.chadpizza.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -25,11 +26,13 @@ public class CustomerController {
 
 	private final CustomerDAO customerDAO;
 
+	@CrossOrigin(origins = "http://localhost:3000")
 	@GetMapping
 	public ResponseEntity<List<Customer>> getAll() {
 		return new ResponseEntity<>(customerDAO.getAll(), HttpStatus.OK);
 	}
 
+	@CrossOrigin(origins = "http://localhost:3000")
 	@GetMapping("/{id}")
 	public ResponseEntity<Customer> get(@PathVariable final Long id) {
 		Customer customer = customerDAO.get(id);
@@ -39,12 +42,14 @@ public class CustomerController {
 		return new ResponseEntity<>(customer, HttpStatus.OK);
 	}
 
+	@CrossOrigin(origins = "http://localhost:3000")
 	@PostMapping
 	public ResponseEntity<Customer> create(@RequestBody final Customer customer) {
 		customerDAO.insert(customer);
 		return new ResponseEntity<>(customer, HttpStatus.OK);
 	}
 
+	@CrossOrigin(origins = "http://localhost:3000")
 	@PutMapping("/{id}")
 	public ResponseEntity<Customer> update(@PathVariable final Long id, @RequestBody final Customer customer) {
 		if (id == null || customerDAO.get(id) == null) {
