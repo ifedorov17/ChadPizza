878a117a84ee1b0ec7568df917513134324c0416
iFedorov <igor170701@gmail.com>
2023-03-12 01:38:14 +0300

    DTO Layer

---
+++ src/main/java/ru/igor17/chadpizza/view/OrderListDTO.java
@@ -0,0 +1,30 @@
+package ru.igor17.chadpizza.view;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class OrderListDTO {
+
+	private Long id;
+
+	private String customerFIO;
+
+	private String customerPhoneNumber;
+
+	private String customerAddress;
+
+	private String orderDateTime;
+
+	private String orderStatus;
+
+	private String orderTotalPrice;
+
+	private List<OrderPositionDTO> orderPositions;
+
+}
---
+++ src/main/java/ru/igor17/chadpizza/view/PizzaAddDTO.java
@@ -0,0 +1,18 @@
+package ru.igor17.chadpizza.view;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class PizzaAddDTO {
+
+	private String name;
+
+	private String description;
+
+	private String price;
+
+}
