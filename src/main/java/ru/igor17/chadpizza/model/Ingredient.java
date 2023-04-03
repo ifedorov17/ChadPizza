16c778f7a6d8f774e9370b2a71ead1f800ab3c12
iFedorov <igor170701@gmail.com>
2023-04-03 15:56:08 +0300

    Ingredient & LnkPizzaIngredient entities for order control

---
+++ src/main/java/ru/igor17/chadpizza/model/Ingredient.java
@@ -0,0 +1,20 @@
+package ru.igor17.chadpizza.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "INGREDIENT")
+public class Ingredient extends BaseModel {
+
+@Column(name = "NAME")
+	private String name;
+
+}
